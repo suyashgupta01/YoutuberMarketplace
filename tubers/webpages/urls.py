a4ecacd8d4090610d6fac0b82aae35458494ebb5
suyash gupta <suyashgupta0001@gmail.com>
2021-01-02 16:53:59 +0530

    webpages visible

--- tubers/webpages/urls.py
+++ tubers/webpages/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.base, name='base'),
+    path('home', views.home, name='home'),
     path('about', views.about,name = 'about'),
     path('services', views.services,name = 'services'),
     path('contact', views.contact,name = 'contact'),
